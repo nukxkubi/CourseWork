938514264dd643a2e68ad944a0cd61f71b20eb3b
nukxkubi <130269237+nukxkubi@users.noreply.github.com>
2024-11-19 01:21:17 +1100

    Adding a drop-down list
    Adding a drop-down list in the section: Categories.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AutoPartsSellerApp
+namespace AutoPartsSellerApp
 {
     partial class Form1
     {
@@ -9,6 +9,11 @@
         private System.Windows.Forms.Button btnSettings; // Кнопка "Настройки"
         private System.Windows.Forms.Button btnHelp; // Кнопка "Справка"
 
+        private System.Windows.Forms.Label lblSelectCategory; // Текст "Выберите категорию"
+        private System.Windows.Forms.ComboBox cbCategories; // Выпадающий список категорий
+        private System.Windows.Forms.Label lblSelectSubCategory; // Текст "Выберите подкатегорию"
+        private System.Windows.Forms.ComboBox cbSubCategories; // Выпадающий список подкатегорий
+
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
@@ -18,11 +23,20 @@
             this.btnSettings = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
 
+            this.lblSelectCategory = new System.Windows.Forms.Label();
+            this.cbCategories = new System.Windows.Forms.ComboBox();
+            this.lblSelectSubCategory = new System.Windows.Forms.Label();
+            this.cbSubCategories = new System.Windows.Forms.ComboBox();
+
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1440, 900);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.lblSelectCategory);
+            this.Controls.Add(this.cbCategories);
+            this.Controls.Add(this.lblSelectSubCategory);
+            this.Controls.Add(this.cbSubCategories);
             this.Name = "Form1";
             this.Text = "АРМ Продавца Автозапчастей";
 
@@ -77,6 +91,44 @@
             this.btnHelp.Height = 50;
             this.btnHelp.Text = "Справка";
             this.btnHelp.Name = "btnHelp";
+
+            // 
+            // lblSelectCategory
+            // 
+            this.lblSelectCategory.AutoSize = true;
+            this.lblSelectCategory.Location = new System.Drawing.Point(220, 30);
+            this.lblSelectCategory.Name = "lblSelectCategory";
+            this.lblSelectCategory.Size = new System.Drawing.Size(200, 20);
+            this.lblSelectCategory.Text = "Выберите категорию запчасти:";
+            this.lblSelectCategory.Visible = false;
+
+            // 
+            // cbCategories
+            // 
+            this.cbCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategories.Location = new System.Drawing.Point(220, 60);
+            this.cbCategories.Name = "cbCategories";
+            this.cbCategories.Size = new System.Drawing.Size(300, 28);
+            this.cbCategories.Visible = false;
+
+            // 
+            // lblSelectSubCategory
+            // 
+            this.lblSelectSubCategory.AutoSize = true;
+            this.lblSelectSubCategory.Location = new System.Drawing.Point(220, 110);
+            this.lblSelectSubCategory.Name = "lblSelectSubCategory";
+            this.lblSelectSubCategory.Size = new System.Drawing.Size(200, 20);
+            this.lblSelectSubCategory.Text = "Выберите что именно вас интересует:";
+            this.lblSelectSubCategory.Visible = false;
+
+            // 
+            // cbSubCategories
+            // 
+            this.cbSubCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSubCategories.Location = new System.Drawing.Point(220, 140);
+            this.cbSubCategories.Name = "cbSubCategories";
+            this.cbSubCategories.Size = new System.Drawing.Size(300, 28);
+            this.cbSubCategories.Visible = false;
         }
     }
 }
