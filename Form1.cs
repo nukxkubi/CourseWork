b51a0a8b395e5d9efdfc267c3ebf6649a2f2e6da
nukxkubi <130269237+nukxkubi@users.noreply.github.com>
2024-11-19 01:23:34 +1100

    Adding a drop-down list
    Adding a drop-down list in the section: Categories. A condition has also been added where the second drop-down list is not active until the first list is selected.

--- Form1.cs
+++ Form1.cs
@@ -1,44 +1,87 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace AutoPartsSellerApp
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, List<string>> categories;
+
         public Form1()
         {
             InitializeComponent();
-            // Добавление обработчика событий
+            InitializeData();
+            InitializeEvents();
+        }
+
+        // Инициализация данных
+        private void InitializeData()
+        {
+            // Категории и подкатегории
+            // TODO: Подключить базу данных для полного списка запчастей.
+            
+            categories = new Dictionary<string, List<string>>()
+            {
+                { "Двигатель", new List<string> { "Поршни", "Масляный фильтр", "Ремень ГРМ" } },
+                { "Подвеска", new List<string> { "Амортизаторы", "Пружины", "Стабилизатор" } },
+                { "Кузов", new List<string> { "Бампер", "Капот", "Зеркала" } }
+            };
+
+            cbCategories.Items.AddRange(categories.Keys.ToArray());
+        }
+
+        // Инициализация событий
+        private void InitializeEvents()
+        {
             btnCategories.Click += BtnCategories_Click;
             btnStock.Click += BtnStock_Click;
             btnOrder.Click += BtnOrder_Click;
-            btnSettings.Click += BtnSettings_Click;
-            btnHelp.Click += BtnHelp_Click;
+            cbCategories.SelectedIndexChanged += CbCategories_SelectedIndexChanged;
         }
 
+        // Событие: кнопка "Категории"
         private void BtnCategories_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Открыта категория товаров!");
+            ToggleCategoryControls(true);
         }
 
+        // Событие: кнопка "Склад"
         private void BtnStock_Click(object sender, EventArgs e)
         {
+            ToggleCategoryControls(false);
             MessageBox.Show("Просмотр склада!");
         }
 
+        // Событие: кнопка "Оформление заказа"
         private void BtnOrder_Click(object sender, EventArgs e)
         {
+            ToggleCategoryControls(false);
             MessageBox.Show("Оформление заказа!");
         }
 
-        private void BtnSettings_Click(object sender, EventArgs e)
+        // Событие: выбор категории
+        private void CbCategories_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MessageBox.Show("Открыты настройки!");
+            cbSubCategories.Items.Clear();
+            cbSubCategories.Enabled = false;
+
+            if (cbCategories.SelectedIndex != -1)
+            {
+                string selectedCategory = cbCategories.SelectedItem.ToString();
+                cbSubCategories.Items.AddRange(categories[selectedCategory].ToArray());
+                cbSubCategories.Enabled = true;
+            }
         }
 
-        private void BtnHelp_Click(object sender, EventArgs e)
+        // Включение/выключение контролов
+        private void ToggleCategoryControls(bool isVisible)
         {
-            MessageBox.Show("Справка по приложению!");
+            lblSelectCategory.Visible = isVisible;
+            cbCategories.Visible = isVisible;
+            lblSelectSubCategory.Visible = isVisible;
+            cbSubCategories.Visible = isVisible;
         }
     }
 }
