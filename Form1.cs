b62f87ed3172ff963cb627cf6131d7a2f8dc3b3c
nukxkubi <130269237+nukxkubi@users.noreply.github.com>
2024-11-19 03:00:55 +1100

    Replacement of spare parts for the car
    Now, when you select a certain brand of car, its model appears.

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ namespace AutoPartsSellerApp
 {
     public partial class Form1 : Form
     {
-        private Dictionary<string, List<string>> categories;
+        private Dictionary<string, List<string>> carBrandsAndModels;
 
         public Form1()
         {
@@ -19,18 +19,24 @@ namespace AutoPartsSellerApp
         // Инициализация данных
         private void InitializeData()
         {
-            // Категории и подкатегории
-            // TODO: Подключить базу данных для полного списка запчастей.
-            
-            categories = new Dictionary<string, List<string>>()
+            carBrandsAndModels = new Dictionary<string, List<string>>()
             {
-                { "Двигатель", new List<string> { "Поршни", "Масляный фильтр", "Ремень ГРМ" } },
-                { "Подвеска", new List<string> { "Амортизаторы", "Пружины", "Стабилизатор" } },
-                { "Кузов", new List<string> { "Бампер", "Капот", "Зеркала" } }
+                { "Toyota", new List<string> { "Camry", "Corolla", "RAV4", "Land Cruiser" } },
+                { "Лада", new List<string> { "Гранта", "Приора", "Веста", "2107" } },
+                { "Nissan", new List<string> { "X-Trail", "Note", "Juke", "Teana" } },
+                { "Honda", new List<string> { "Fit", "CR-V", "HR-V", "Accord" } },
+                { "Hyundai", new List<string> { "Solaris", "Santa Fe", "Tucson", "Creta", "Accent" } },
+                { "Audi", new List<string> { "A6", "A4", "Q7", "Q5", "A3", "A7", "A8", "Q8" } },
+                { "BMW", new List<string> { "5-Series", "X5", "3-Series", "X6", "X3", "M3", "M5", "X1" } },
+                { "Lexus", new List<string> { "RX350", "RX300", "LX570", "GX460", "RX450h", "GX470", "ES300" } },
+                { "Mazda", new List<string> { "Mazda3", "Mazda6", "CX-5", "Demio", "Axela", "RX-7", "RX-8" } },
+                { "Mercedes-Benz", new List<string> { "E-Class", "C-Class", "S-Class", "M-Class", "G-Class", "S-Class" } },
+                { "Mitsubishi", new List<string> { "Outlander", "Lancer", "Pajero", "Pajero Sport", "ASX", "Delica", "Galant", "Mirage" } }
             };
 
-            cbCategories.Items.AddRange(categories.Keys.ToArray());
+            cbCarBrands.Items.AddRange(carBrandsAndModels.Keys.ToArray());
         }
+
 
         // Инициализация событий
         private void InitializeEvents()
@@ -38,50 +44,59 @@ namespace AutoPartsSellerApp
             btnCategories.Click += BtnCategories_Click;
             btnStock.Click += BtnStock_Click;
             btnOrder.Click += BtnOrder_Click;
-            cbCategories.SelectedIndexChanged += CbCategories_SelectedIndexChanged;
+            cbCarBrands.SelectedIndexChanged += CbCarBrands_SelectedIndexChanged;
         }
 
         // Событие: кнопка "Категории"
         private void BtnCategories_Click(object sender, EventArgs e)
         {
-            ToggleCategoryControls(true);
+            ToggleCarSelectionControls(true);
         }
 
         // Событие: кнопка "Склад"
         private void BtnStock_Click(object sender, EventArgs e)
         {
-            ToggleCategoryControls(false);
+            ToggleCarSelectionControls(false);
             MessageBox.Show("Просмотр склада!");
         }
 
         // Событие: кнопка "Оформление заказа"
         private void BtnOrder_Click(object sender, EventArgs e)
         {
-            ToggleCategoryControls(false);
+            ToggleCarSelectionControls(false);
             MessageBox.Show("Оформление заказа!");
         }
 
-        // Событие: выбор категории
-        private void CbCategories_SelectedIndexChanged(object sender, EventArgs e)
+        // Событие: выбор марки автомобиля
+        private void CbCarBrands_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cbSubCategories.Items.Clear();
-            cbSubCategories.Enabled = false;
+            cbCarModels.Items.Clear(); // Очищаем список моделей
+            cbCarModels.Enabled = false; // Отключаем ComboBox для моделей
 
-            if (cbCategories.SelectedIndex != -1)
+            if (cbCarBrands.SelectedIndex != -1)
             {
-                string selectedCategory = cbCategories.SelectedItem.ToString();
-                cbSubCategories.Items.AddRange(categories[selectedCategory].ToArray());
-                cbSubCategories.Enabled = true;
+                string selectedBrand = cbCarBrands.SelectedItem?.ToString();  // Получаем выбранную марку
+
+                if (!string.IsNullOrEmpty(selectedBrand) && carBrandsAndModels.ContainsKey(selectedBrand))
+                {
+                    // Добавляем модели в cbCarModels
+                    cbCarModels.Items.AddRange(carBrandsAndModels[selectedBrand].ToArray());
+                    cbCarModels.Enabled = true; // Включаем ComboBox для моделей
+                }
+                else
+                {
+                    MessageBox.Show("Не найдено моделей для выбранной марки.");
+                }
             }
         }
 
-        // Включение/выключение контролов
-        private void ToggleCategoryControls(bool isVisible)
+        // Включение/выключение контролов выбора марки и модели автомобиля
+        private void ToggleCarSelectionControls(bool isVisible)
         {
-            lblSelectCategory.Visible = isVisible;
-            cbCategories.Visible = isVisible;
-            lblSelectSubCategory.Visible = isVisible;
-            cbSubCategories.Visible = isVisible;
+            lblSelectCarBrand.Visible = isVisible;
+            cbCarBrands.Visible = isVisible;
+            lblSelectCarModel.Visible = isVisible;
+            cbCarModels.Visible = isVisible;
         }
     }
 }
